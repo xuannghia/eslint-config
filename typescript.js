1483edcc9c1c9bda10485a502189c176bb2ef376
xuannghia <xuannghia197@gmail.com>
2024-02-21 21:09:12 +0700

    add parser & format with Prettier

--- typescript.js
+++ typescript.js
@@ -2,14 +2,25 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended'
   ],
   plugins: ['@typescript-eslint', 'simple-import-sort'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: process.cwd(),
+  },
   rules: {
     // Format
-    'indent': ['error', 2, { SwitchCase: 1 }],
-    'semi': ['error', 'never'],
-    'quotes': ['error', 'single'],
-    'comma-dangle': ['error', 'always-multiline'],
+    "prettier/prettier": ["error", {
+      "printWidth": 120,
+      "endOfLine": "auto",
+      "singleQuote": true,
+      "semi": false,
+      "tabWidth": 2,
+      "trailingComma": "all",
+    }],
     // Lint
     'simple-import-sort/imports': 'error',
     'simple-import-sort/exports': 'error',
